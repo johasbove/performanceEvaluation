c959288d3e0ed58392c42c171b2c9fa96e132130
Johanna Salazar Bove <johanna.salazar.b@gmail.com>
2019-01-04 15:34:22 -0300

    Add cors. Add basic rails views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :allow_cross_domain_ajax
+
+  def allow_cross_domain_ajax
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Request-Method'] = '*'
+  end
 end
